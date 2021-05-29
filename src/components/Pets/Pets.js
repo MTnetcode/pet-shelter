91086a1781988bd4344206313af99d676eddaf11
Tomas Storc <tomas.storc@gmail.com>
2021-05-29 09:32:59 +0200

    remove warnings

--- src/components/Pets/Pets.js
+++ src/components/Pets/Pets.js
@@ -6,11 +6,11 @@ import fetchPets from "./fetchPets.js";
 function Pets({ category }) {
   const [getPets, setPets] = useState([]);
 
-  async function returnPets() {
-    const pets = await fetchPets(category);
-    setPets(pets);
-  }
   useEffect(() => {
+    async function returnPets() {
+      const pets = await fetchPets(category);
+      setPets(pets);
+    }
     returnPets();
   }, [category]);
 
