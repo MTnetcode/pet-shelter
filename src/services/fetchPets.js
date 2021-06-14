376d08b0464184f3d9b29d66aba09cd89fec25f4
Tomas Storc <tomas.storc@gmail.com>
2021-06-14 16:04:27 +0200

    added comments

--- src/services/fetchPets.js
+++ src/services/fetchPets.js
@@ -1,5 +1,10 @@
 const SERVER = "http://petshelter-api.mtnetcode.com";
 
+/**
+ * function to fetch pets from server
+ * @param {string} category - should be dogs or cats right now
+ * @returns string array with pets from given category
+ */
 export default async function fetchPets(category) {
   const res = await fetch(`${SERVER}/api/pets/${category}`);
   const data = await res.json();
--- src/services/sendData.js
+++ src/services/sendData.js
@@ -1,5 +1,10 @@
 const SERVER = "http://petshelter-api.mtnetcode.com";
 
+/**
+ * function which saves new pet to db
+ * @param {HTMLFormElement} data - form data to save
+ * @returns JSON with message from server
+ */
 export default async function sendData(data) {
   const res = await fetch(`${SERVER}/api/pets`, {
     method: "POST",
