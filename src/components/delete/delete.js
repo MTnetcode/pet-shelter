ff5dc35f4883302f267829f9a9050a80b3af2f15
Tomas Storc <tomas.storc@gmail.com>
2021-06-27 09:04:53 +0200

    delete post

--- src/components/delete/delete.js
+++ src/components/delete/delete.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./delete.css";
-function Delete() {
+function Delete({ deletePet, id, setOpenDelete }) {
   return (
     <div className="delete">
-        <h1 className="h1-delete">
-        Are you sure you want to delete this post?
-        </h1>
-        
-        <button className="btn-delete">
-            Yes
-        </button>
-        <button className="btn-delete">
-            No
-        </button>
+      <h1 className="h1-delete">Are you sure you want to delete this post?</h1>
 
+      <button
+        className="btn-delete"
+        onClick={() => {
+          deletePet(id);
+          setOpenDelete(false);
+        }}
+      >
+        Yes
+      </button>
+      <button
+        className="btn-delete"
+        onClick={() => {
+          setOpenDelete(false);
+        }}
+      >
+        No
+      </button>
     </div>
   );
 }
--- src/components/pets-dashboard/PetsBoxDashboard.js
+++ src/components/pets-dashboard/PetsBoxDashboard.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useState } from "react";
+import Delete from "../delete/delete";
 import "./petsDashboard.css";
-function PetsBoxDashboard({ id, img, name, text }) {
-  console.log(name);
+function PetsBoxDashboard({ id, img, name, text, deletePet }) {
+  const [openDelete, setOpenDelete] = useState(false);
   return (
     <div className="pet-flex">
       <div className="img-div">
@@ -10,11 +12,19 @@ function PetsBoxDashboard({ id, img, name, text }) {
       <div className="info">
         <div className="change-icons">
           <i class="fas fa-pen fa-2x fachange"></i>
-          <i class="fas fa-2x fa-trash fachange"></i>
+          <i
+            class="fas fa-2x fa-trash fachange"
+            onClick={() => {
+              setOpenDelete(true);
+            }}
+          ></i>
         </div>
         <h2 className="name">{name}</h2>
         <p className="pet-paragraph">{text}</p>
       </div>
+      {openDelete && (
+        <Delete deletePet={deletePet} id={id} setOpenDelete={setOpenDelete} />
+      )}
     </div>
   );
 }
