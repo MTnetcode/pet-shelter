7758bcea72d097300a44c38bbfdad44a7089c692
Tomas Storc <tomas.storc@gmail.com>
2021-06-27 10:23:33 +0200

    redirect to login when not logged in

--- src/components/pets-dashboard/PetsDashboard.js
+++ src/components/pets-dashboard/PetsDashboard.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
 import "./petsDashboard.css";
 import PetsBoxDashboard from "./PetsBoxDashboard.js";
 import Post from "../post/post";
 import fetchPets from "../../services/fetchPets";
 import deletePost from "../../services/deletePost";
+import verifyLogin from "../../services/verifyLogin";
+import Wait from "../reusable/Wait";
 
 function PetsDashboard({ category }) {
   const [addNew, setAddNew] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(loginVerifyService());
   const [getPets, setPets] = useState([]);
   useEffect(() => {
     async function returnPets() {
@@ -14,7 +18,7 @@ function PetsDashboard({ category }) {
       setPets(pets);
     }
     returnPets();
-  }, [category, addNew === false]);
+  }, [category, addNew]);
 
   const handleClick = (e) => {
     setAddNew(!addNew);
@@ -23,6 +27,11 @@ function PetsDashboard({ category }) {
   function deletePet(id) {
     setPets(getPets.filter((pet) => pet._id !== id));
     deletePost(id);
+  }
+
+  async function loginVerifyService() {
+    const res = await verifyLogin();
+    setIsLoggedIn(res);
   }
   return (
     <div>
@@ -37,19 +46,22 @@ function PetsDashboard({ category }) {
         />
       )}
       <div className="pets">
-        {getPets.length > 0
-          ? getPets.map((pet) => (
-              <PetsBoxDashboard
-                key={pet._id}
-                id={pet._id}
-                img={pet.img}
-                name={pet.name}
-                text={pet.text}
-                deletePet={deletePet}
-              />
-            ))
-          : "getting pets from server"}
+        {getPets.length > 0 ? (
+          getPets.map((pet) => (
+            <PetsBoxDashboard
+              key={pet._id}
+              id={pet._id}
+              img={pet.img}
+              name={pet.name}
+              text={pet.text}
+              deletePet={deletePet}
+            />
+          ))
+        ) : (
+          <Wait msg="getting pets" />
+        )}
       </div>
+      {!isLoggedIn && <Redirect to="/login" />}
     </div>
   );
 }
