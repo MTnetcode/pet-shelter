7252453b976e66b50acd4404f1d4eedb678b056a
Tomas Storc <tomas.storc@gmail.com>
2021-06-29 19:21:32 +0200

    show info

--- src/components/Pets/PetsBox.js
+++ src/components/Pets/PetsBox.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./pets.css";
+import PetsInfo from "../pets-info/petsInfo";
 function PetsBox({ id, img, name, text }) {
+  const [showInfo, setShowInfo] = useState(false);
   return (
     <div className="pet-flex">
+      {showInfo && (
+        <PetsInfo img={img} name={name} text={text} setShowInfo={setShowInfo} />
+      )}
       <div className="img-div">
         <img className="img" src={img} alt="" />
       </div>
@@ -10,7 +15,13 @@ function PetsBox({ id, img, name, text }) {
         <h2 className="name">{name}</h2>
         <p className="pet-paragraph">
           {text.slice(0, 200)}...
-          <em>more</em>{" "}
+          <em
+            onClick={() => {
+              setShowInfo(!showInfo);
+            }}
+          >
+            more
+          </em>{" "}
         </p>
       </div>
     </div>
